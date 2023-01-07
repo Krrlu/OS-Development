d2bfc71f1257f63c93f5e9faa8fed73cb7c07bae
Karry Liu <kla251@sfu.ca>
2023-01-07 15:32:24 -0800

    Implement print_string function

--- include/print.h
+++ include/print.h
@@ -1,3 +1,6 @@
 #define VGA_BUFFER (char*)0xb8000
 #define VGA_WIDTH 80 
+#define  TEXT_ATTRIBUTE 7
+
 void print_char(char c);
+void print_string(char* s);
--- print.c
+++ print.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include "print.h"
 
-uint16_t get_cursor_position(){
+uint16_t static inline get_cursor_position(){
     uint16_t pos = 0;
 
     outb(0x0F, 0x3D4);
@@ -14,9 +14,8 @@ uint16_t get_cursor_position(){
     return pos;
 }
 
-void set_cursor(int x, int y)
+void static inline set_cursor(uint16_t pos)
 {
-	uint16_t pos = y * VGA_WIDTH + x;
  
 	outb(0x0F, 0x3D4);
 	outb((uint8_t)(pos & 0xFF),0x3D5);
@@ -24,11 +23,32 @@ void set_cursor(int x, int y)
 	outb((uint8_t)((pos >> 8) & 0xFF),0x3D5);
 }
 
+void print_string(char* s){
+    char* buffer = VGA_BUFFER;
+    uint32_t pos = (uint32_t)(get_cursor_position()); //offset in VGA buffer
+    while(*s){
+        if(*s == '\n'){
+        pos = ((pos / VGA_WIDTH) + 1) * VGA_WIDTH;
+    }else{
+        *(buffer + (pos << 1)) = *s; //character
+        *(buffer + (pos << 1) + 1) = TEXT_ATTRIBUTE;
+    pos++;
+    }
+    s++;
+    }
+    set_cursor(pos);
+}
 
 void print_char(char c){
     
     char* buffer = VGA_BUFFER;
-    uint32_t pos = ((uint32_t)(get_cursor_position())) << 1; //offset in VGA buffer
-    *(buffer + pos) = c; //character
-    *(buffer + pos+1) = 7; //color
+    uint32_t pos = (uint32_t)(get_cursor_position()); //offset in VGA buffer
+    if(c == '\n'){
+        pos = ((pos / VGA_WIDTH) + 1) * VGA_WIDTH;
+    }else{
+        *(buffer + (pos << 1)) = c; //character
+        *(buffer + (pos << 1) + 1) = TEXT_ATTRIBUTE;
+    pos++;
+    }
+    set_cursor(pos);
 }
