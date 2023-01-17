29fcf99cfe2caefed7137d1065eec3da6e5cbcc8
Karry Liu <kla251@sfu.ca>
2023-01-17 15:42:30 -0800

    Update script to get size of the bin file

--- update_VHD.sh
+++ update_VHD.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+size=$(wc -c < kernel.bin)
+sectors=$((size/512+1))
 
 dd if=boot.bin of=$1 bs=512 count=1 conv=notrunc #update bootloader
-dd if=kernel.bin of=$1 bs=512 count=3 seek=1 conv=notrunc #update kernel
+dd if=kernel.bin of=$1 bs=512 count=$sectors seek=1 conv=notrunc #update kernel
