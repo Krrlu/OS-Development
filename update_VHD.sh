7db5d8bf83a07d3570f6c0a6b701a0dd6e67dfd3
Karry Liu <kla251@sfu.ca>
2023-02-09 20:37:05 -0800

    Update file locations

--- update_VHD.sh
+++ update_VHD.sh
@@ -2,5 +2,5 @@
 size=$(wc -c < kernel.bin)
 sectors=$((size/512+1))
 
-dd if=boot.bin of=$1 bs=512 count=1 conv=notrunc #update bootloader
-dd if=kernel.bin of=$1 bs=512 count=$sectors seek=1 conv=notrunc #update kernel
+dd if=./bin/boot.bin of=$1 bs=512 count=1 conv=notrunc #update bootloader
+dd if=./bin/kernel.bin of=$1 bs=512 count=$sectors seek=1 conv=notrunc #update kernel
